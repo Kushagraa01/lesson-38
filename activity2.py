a3592c552d1e44d357192c17be60cc6d3fdcb4d7
Kushagraa01 <swatiabhijain@gmail.com>
2025-02-16 18:07:52 +0530

    lesson 38 activity2

---
+++ activity2.py
@@ -0,0 +1,18 @@
+num1=20
+num2=3
+
+print("Sum of",num1,"and",num2,"equal to",num1+num2)
+print("Difference of",num1,"and",num2,"equal to",num1-num2)
+print("Product of",num1,"and",num2,"equal to",num1*num2)
+print("Division of",num1,"and",num2,"equal to",num1/num2)
+print("Floor Division of",num1,"and",num2,"equal to",num1//num2)
+print("Modulur of",num1,"and",num2,"equal to",num1%num2)
+print("2 to power of 5",2**5)
+
+print("is",num1,"equal to",num2,num1==num2)
+print("is",num1,"greater than",num2,num1>num2)
+print("is",num1,"less than",num2,num1<num2)
+
+result=num1/2+num2*10+17
+
+print("Result is",result)
