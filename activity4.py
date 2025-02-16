266f1be41913f0abe658fa6fdc64210790104799
Kushagraa01 <swatiabhijain@gmail.com>
2025-02-16 18:45:28 +0530

    lesson 38 activity4

---
+++ activity4.py
@@ -0,0 +1,12 @@
+num1=input("Enter first number")
+num2=input("Enter second number")
+
+print("Number before swaping ",num1)
+print("Number before swaping ",num2)
+
+temp=num1
+num1=num2
+num2=temp
+
+print("Number after swaping ",num1)
+print("Number after swaping ",num2)
