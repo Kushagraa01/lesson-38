fefda37fe88c37606b5965cae2f7f2814bcef38b
Kushagraa01 <swatiabhijain@gmail.com>
2025-02-16 18:26:55 +0530

    lesson 38 activity 3

---
+++ activity3.py
@@ -0,0 +1,14 @@
+firstname="Kushagraa"
+lastname="Jain"
+
+fullname=firstname+" "+lastname
+
+print("Firstname is ",firstname)
+print("Lastname is ",lastname)
+print("Fullname is ",fullname)
+print("Firstname multiplied by 4",firstname*4)
+
+print("Length of fullname is ",len(fullname))
+print("First character of fullname is ",fullname[0])
+print("Last character of fullname is ",fullname[-1])
+print("Sliced name is",fullname[2:8])
